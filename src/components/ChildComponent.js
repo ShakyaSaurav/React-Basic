bcaed0d2ac07ae591a10595bd17bbf8f1f16e9bd
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-12-03 17:20:48 +0545

    parent and child binding

---
+++ src/components/ChildComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChildComponent() {
+  return (
+    <div><button>Greet Parents</button></div>
+  )
+}
+
+export default ChildComponent
---
+++ src/components/ParentComponent.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import ChildComponent from './ChildComponent'
+
+class ParentComponent extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         parentName: 'Parent'
+      }
+      this.greetParent= this.greetParent.bind(this)
+    }
+    greetParent(){
+        alert(`Hello ${this.state.parentName}`)
+    }
+  render() {
+    return (
+      <div>
+        <ChildComponent/>
+        
+        </div>
+    )
+  }
+}
+
+export default ParentComponent
