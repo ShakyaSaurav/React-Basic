5fa9dbb88b5d22924f448bab250fc6be65a7fc19
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-12-20 21:11:43 +0545

    basic form data

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
-  render() {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         username:'',
+         comments:'',
+         Topic:'',
+      }
+    }
+    handleUsernameChange=(event)=>{
+        this.setState({
+            username: event.target.value
+        })
+    }
+    handleCommentsChange=(event)=>{
+        this.setState({
+            comments: event.target.value
+        })
+    }
+    handleTopicChange=(event)=>{
+        this.setState({
+            topic:event.target.value
+        })
+    }
+     
+    render(){
     return (
       <form>
         <div>
             <label>Username</label>
-            <input type='text'/>
+            <input type='text' value= {this.state.username} onChange={this.handleUsernameChange}/>
+        </div>
+        <div>
+            <label>Comments</label>
+            <textarea value={this.state.comments} onChange={this.handleCommentsChange}></textarea>
+        </div>
+        <div>
+            <label>Faculty</label>
+            <select value={this.state.topic} onChange={this.handleTopicChange}>
+            <option value='react'>React</option>
+            <option value='angular'>Angular</option>
+            <option value='Vue'>Vue</option>
+            </select>
         </div>
       </form>
     )
